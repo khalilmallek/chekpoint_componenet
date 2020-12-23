f60d851f69e56d8445c93296b6ad0741cf31f939
khalilmallek <dskhalilmallek@gmail.com>
2020-12-23 10:10:55 +0100

    first commit

---
+++ src/Components/Profile/Adress.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Adress = () => {
+    return (
+        <div>
+            <h3>adress</h3>
+        </div>
+    )
+}
+
+export default Adress
---
+++ src/Components/Profile/ProfilePhoto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProfilePhoto = () => {
+    return (
+        <div>
+            <img src='https://www.pngitem.com/pimgs/m/78-786293_1240-x-1240-0-avatar-profile-icon-png.png' alt='img1'/>
+        </div>
+    )
+}
+
+export default ProfilePhoto
